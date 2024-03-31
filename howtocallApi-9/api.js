120088e23ea0e856158e891726d66bb3d2fc0821
Manish kumar <manishkumar22082000@gmail.com>
2024-03-31 15:13:00 +0530

    new change

---
+++ howtocallApi-9/api.js
@@ -0,0 +1,39 @@
+
+// we fetch the data by API  1 method  
+
+// let  xhr = new XMLHttpRequest()
+
+// xhr.onreadystatechange = function(){
+//     if(this.readyState == 4 && this.status == 200){
+//         console.log(this.responseText)
+//     }
+// }
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
+// xhr.send()
+
+
+// we fetch the data by API  2  method  using fetch 
+
+// fetch('https://jsonplaceholder.typicode.com/users', {
+//     method: 'GET'
+// })
+// .then(res => res.json())
+// .then((data) => {
+//     console.log(data)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
